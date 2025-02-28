baee3cda3b36da9d66f5e85750a737c84cf70444
Eugene P. <pinaevmsk@gmail.com>
2025-02-28 16:41:35 +0400

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
+            return a / b;
+        }
+    }
+}
